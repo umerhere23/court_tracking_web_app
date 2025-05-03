348ff787bae1a61e975a459804193dcbb98315ab
Frank <frank.snelling03@gmail.com>
2025-05-03 16:16:09 +0100

    Basic Application
    Created singleton, connection to database and defined paths.

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+/* SET to display all warnings in development. Comment next two lines out for production mode*/
+ini_set('display_errors','On');
+error_reporting(E_ERROR | E_PARSE);
+
+/* path to Lib */
+DEFINE("LIB",$_SERVER['DOCUMENT_ROOT']."/../lib/");
+
+/* path to views and partials */
+// DEFINE("VIEWS",LIB."views/");
+// DEFINE("PARTIALS",VIEWS."/partials");
+
+/* path to MODEL and APP */
+DEFINE("MODEL",LIB."/model.php");
+DEFINE("APP",LIB."/application.php");
+
+/* define layout */
+// DEFINE("LAYOUT","standard");
+
+require APP;
