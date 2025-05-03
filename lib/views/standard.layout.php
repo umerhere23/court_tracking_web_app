6d74a14f5ba3123c1f04a8a9c7a8fb08c0723a3d
HoustonMichaelVaughn <s377726@students.cdu.edu.au>
2025-05-04 08:43:43 +0930

    Added searching features

--- lib/views/standard.layout.php
+++ lib/views/standard.layout.php
@@ -3,18 +3,16 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Court Outcome Tracking</title>
   </head>
   <body>
-  <?php require PARTIALS . "/navbar.php"; ?>
+    <?php require PARTIALS . "/navbar.php"; ?>
 
-  <div class="container mt-4">
-      <?php require VIEWS . "/{$content}.php"; ?>
+    <div class="container mt-4">
+        <?php require VIEWS . "/{$content}.view.php"; ?>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
   </body>
-
-  
+</html>
--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors','On');
+error_reporting(E_ERROR | E_PARSE);
+
 require_once '../lib/includes/mouse.php';
 
 get('/', function($app) {
@@ -7,6 +10,11 @@ get('/', function($app) {
 
 get('/search', function($app) {
     require_once '../lib/includes/search_controller.php';
+});
+
+// Route: Add defendant (from your friend)
+get("/defendant/add", function($app) {
+    return ($app->render)("standard", "defendant_form");
 });
 
 resolve();
