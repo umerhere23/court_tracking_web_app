a25f97d1143d9aea809cbdff4443ad9e8f0f745b
Micia Gusmao <gusmaomicia@gmail.com>
2025-05-20 17:05:29 +0930

    Edit login.view.php

--- lib/views/authentication/login.view.php
+++ lib/views/authentication/login.view.php
@@ -1,5 +1,5 @@
-<form action="/lib/includes/auth_controller.php" method="post">
-    <input type="text" name="username" placeholder="Username" required>
-    <input type="password" name="password" placeholder="Password" required>
-    <button type="submit">Login</button>
+<form action="<?= BASE_URL ?>/lib/includes/auth_controller.php" method="post">
+  <input type="text" name="username" placeholder="Username" required>
+  <input type="password" name="password" placeholder="Password" required>
+  <button type="submit">Login</button>
 </form>
